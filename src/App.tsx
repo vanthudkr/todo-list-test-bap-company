54a929a19e0a89988bb8054e69d950e211fa5fc0
Văn Thư <vanthudkr@gmail.com>
2023-06-01 19:44:42 +0700

    add some file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { useDispatch } from "react-redux";
 import { fetchTodoRequest } from "./store/todo/actions";
